815520885e02448fd4747b21e1cd426f290a8ba6
RYon <ryanbjo187@gmail.com>
2026-08-13 13:27:21 -0700

    directory organization changes

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,1 @@
-import { defineConfig } from 'vite'; export default defineConfig({ root: 'src', build: { outDir: '../dist' } });
+import { defineConfig } from 'vite'; export default defineConfig({ build: { outDir: '../dist' } });
